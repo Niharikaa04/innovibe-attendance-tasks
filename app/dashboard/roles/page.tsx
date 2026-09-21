0a486f7f7064e909974696c01ae060a816ec6045
niharikahari <niharikahari00@gmail.com>
2026-09-21 11:55:02 +0530

    Remove office portal and keep attendance tasks

--- app/dashboard/roles/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import { RoleAccessGovernanceModule } from '../../../components/ceo/role-access/RoleAccessGovernanceModule';
-
-export default function RoleAccessControlPage() {
-  return <RoleAccessGovernanceModule />;
-}
